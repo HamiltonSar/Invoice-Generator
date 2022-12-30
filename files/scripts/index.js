a5b159b61baee7ab714f0144dc1b62611bd67553
HamiltonSar <hamiltonsarmientopacheco@gmail.com>
2022-12-29 23:15:52 -0500

    Responsive

--- files/scripts/index.js
+++ files/scripts/index.js
@@ -10,14 +10,19 @@ const BodyDataContainer = document.getElementById("BodyDataContainer");
 const DeleteAllButton = document.getElementById("DeleteAllButton");
 const FooterDataContainer = document.getElementById("FooterDataContainer");
 const CalculateButton = document.getElementById("CalculateButton");
+const ResultsMainContainer = document.getElementById("ResultsMainContainer");
+const ResultContainer = document.getElementById("ResultContainer");
+const PrintButton = document.getElementById("PrintButton");
+const ButtonsManagerContainer = document.getElementById("ButtonsManagerContainer");
 let ArrayPrices = [];
 let prices;
 
-CalculateButton.addEventListener("click" , GenerateResults);
+
 
 function GenerateRowElements () {
     PopUpProductManager.style.display = "none";
     TableContainer.style.display = "block";
+
     const createRow = document.createElement("TR");
     createRow.className = "productmanagercontainer__rowbody";
     createRow.innerHTML = 
@@ -33,17 +38,51 @@ function GenerateRowElements () {
     NumbersEntry.value = "";
     UnitaryEntry.value = "";
     PriceEntry.value = "";
+
     let PricesParsed = parseFloat(prices);
     ArrayPrices = [...ArrayPrices , PricesParsed];
-    for (const iteration of ArrayPrices){
-        console.log(ArrayPrices += iteration);
-    }
-
+    
     return BodyDataContainer.appendChild(createRow);
 }
-function GenerateResults () {
-    FooterDataContainer.style.display = "block";
-}
+
+
+
+
+function GenerateCalculation () {
+    ResultsMainContainer.style.display = "flex";
+    let totalSumArray = 0;
+    ArrayPrices.forEach( ( element ) => { totalSumArray += element } );
+    let taxes = parseInt((totalSumArray * 0.2).toFixed(2));
+    let IVA = parseInt((totalSumArray * 0.12).toFixed(2));
+    let Off = parseInt((totalSumArray * 0.10).toFixed(2));
+    let finalPrice = (totalSumArray - Off) + (IVA + taxes);
+
+    ResultContainer.innerHTML = 
+    `
+        <div class="productmanagercontainer__resultcontent">
+            <h2 class="productmanagercontainer__results">Total</h2>
+            <h2 class="productmanagercontainer__results"> ${ totalSumArray }$ </h2>
+        </div>
+        <div class="productmanagercontainer__resultcontent">
+            <h2 class="productmanagercontainer__results">Taxes</h2>
+            <h2 class="productmanagercontainer__results"> ${ taxes }$ </h2>
+        </div>
+        <div class="productmanagercontainer__resultcontent">
+            <h2 class="productmanagercontainer__results">IVA</h2>
+            <h2 class="productmanagercontainer__results"> ${IVA}$ </h2>
+        </div>
+        <div class="productmanagercontainer__resultcontent">
+            <h2 class="productmanagercontainer__results">Off</h2>
+            <h2 class="productmanagercontainer__results">${Off}$</h2>
+        </div>
+        <div class="productmanagercontainer__resultcontent">
+            <h2 class="productmanagercontainer__results">Final</h2>
+            <h2 class="productmanagercontainer__results">${finalPrice}$</h2>
+        </div>
+        `
+        return ResultsMainContainer.innerHTML = createCalculationContainer.innerHTML;
+    }
+
 
 UnitaryEntry.addEventListener("input" , ()=> {
     let NumberParsed = parseFloat(NumbersEntry.value);
@@ -60,11 +99,16 @@ UnitaryEntry.addEventListener("input" , ()=> {
 DeleteAllButton.addEventListener("click" , ()=> {
     BodyDataContainer.innerHTML = "";
 })
-// CalculateButton.addEventListener("click" , ()=> {
-
-// })
 
 AddNewProductButton.addEventListener("click" , ()=> {
     PopUpProductManager.style.display = "flex";
 });
+    
+CalculateButton.addEventListener("click" , GenerateCalculation);
 
+
+PrintButton.addEventListener("click" , ()=> {
+    PopUpProductManager.style.display = "none"
+    ButtonsManagerContainer.style.display = "none";
+    window.print();
+})
